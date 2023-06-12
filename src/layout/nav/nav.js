5befd0eea5e41a031c1bc91073fe3798f0eca2e4
Andrea <andrenefer90@gmail.com>
2023-06-12 13:57:22 +0200

    render del menu

---
+++ src/layout/nav/nav.js
@@ -0,0 +1,19 @@
+class Nav extends Component
+{
+  render()
+  {
+    return (
+      <>
+        <nav>
+			<ul>
+				<li>Inicio</li>
+				<li>Iniciar Sesion</li>
+				<li>Comics</li>
+				<li>Sobre nosotros</li>
+				<li>Contacto</li>
+			</ul>
+		</nav>
+      </>
+    );
+  }
+}
