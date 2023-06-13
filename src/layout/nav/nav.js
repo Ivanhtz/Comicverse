657e0f13b0f91f47b2408f1552fa5a5c8c7a0453
Andrea <andrenefer90@gmail.com>
2023-06-13 08:10:19 +0200

    Actualizado menú y añadida navegación base

--- src/layout/nav/nav.js
+++ src/layout/nav/nav.js
@@ -1,19 +1,30 @@
-class Nav extends Component
-{
-  render()
-  {
-    return (
+import './nav.css';
+import {Link} from 'react-router-dom';
+
+function Nav(){
+	return (
       <>
         <nav>
 			<ul>
-				<li>Inicio</li>
-				<li>Iniciar Sesion</li>
-				<li>Comics</li>
-				<li>Sobre nosotros</li>
-				<li>Contacto</li>
+				<li>
+					<Link to="/">Inicio</Link>
+				</li>
+				<li>
+					<Link to="/login">Iniciar Sesion</Link>
+				</li>
+				<li>
+					<Link to="/comics">Comics</Link>
+				</li>
+				<li>
+					<Link to="/quienes-somos">Sobre nosotros</Link>
+				</li>
+				<li>
+					<Link to="/contacto">Contacto</Link>
+				</li>
 			</ul>
 		</nav>
       </>
     );
-  }
 }
+
+export default Nav;
