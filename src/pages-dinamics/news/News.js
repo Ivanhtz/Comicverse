69248de3daf46d7d7ce0ab8ef3dbeca2fc2d2442
Andrea <andrenefer90@gmail.com>
2023-06-23 11:38:04 +0200

    Arreglado estilo general

--- src/pages-dinamics/news/News.js
+++ src/pages-dinamics/news/News.js
@@ -6,46 +6,46 @@ const { Component } = require("react");
 
 class News extends Component {
 
-    render() {
+	render() {
 
-        return (
-            <section>
-                <header>
-                    <h1 style={{textAlign:"center",color:"white"}}>Lista de Noticias</h1>
-                </header>
-                <div className="content-wrapper">
-                <section style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap' ,}}>
-                {
-                    noticias.map((noticia=>
-                      
-                        <div className="news-card" key={noticia.id}>
-                              <Link to={`/noticias/${noticia.id}`}>
-                            <a className="news-card__card-link"></a>
-                            <img src={noticia.image} alt="" className="news-card__image"/>
-                            <div className="news-card__text-wrapper">
-                            <h2 className="news-card__title">{noticia.title}</h2>
-                            <div className="news-card__post-date">{noticia.date} | {noticia.author}</div>
-                            <div className="news-card__details-wrapper">
-                                <p className="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio, pariatur&hellip;</p>
-                                <Link to={`/noticias/${noticia.id}`}><a style={{textDecoration:"none"}} className="news-card__read-more">Read more <i className="fas fa-long-arrow-alt-right"></i></a></Link>
-                            </div>
-                            </div>
-                            </Link>
-                        </div>
-                        
-                        
-                    ))
-                }
+		return (
+			<div className="newsPage__body">
+				<section className="newsPage__section">
+					<header>
+						<h1 className="newsPage__h1" style={{ textAlign: "center", color: "white" }}>Lista de Noticias</h1>
+					</header>
+					<div className="content-wrapper">
+						<section style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', }}>
+							{
+								noticias.map((noticia =>
 
-                </section>
+									<div className="news-card" key={noticia.id}>
+										<Link to={`/noticias/${noticia.id}`}>
+											<a className="news-card__card-link"></a>
+											<img src={noticia.image} alt="" className="news-card__image" />
+											<div className="news-card__text-wrapper">
+												<h2 className="news-card__title">{noticia.title}</h2>
+												<div className="news-card__post-date">{noticia.date} | {noticia.author}</div>
+												<div className="news-card__details-wrapper">
+													<p className="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio, pariatur&hellip;</p>
+													<Link to={`/noticias/${noticia.id}`}><a style={{ textDecoration: "none" }} className="news-card__read-more">Read more <i className="fas fa-long-arrow-alt-right"></i></a></Link>
+												</div>
+											</div>
+										</Link>
+									</div>
+								))
+							}
 
-                </div> 
+						</section>
 
+					</div>
 
 
-            </section>
-        )
-    }
+
+				</section>
+			</div>
+		)
+	}
 }
 
 export default News;
