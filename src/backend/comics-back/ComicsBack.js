4e8854d1f2fa2933ac88fd7fe9eed675745d9d8a
Andrea <andrenefer90@gmail.com>
2023-06-24 21:00:48 +0200

    Movido estado y lógica al componente padre

--- src/backend/comics-back/ComicsBack.js
+++ src/backend/comics-back/ComicsBack.js
@@ -1,24 +1,47 @@
-import React, { Component } from 'react';
 import ComicsBackForm from './comics-back-form/ComicsBackForm';
 import ComicsBackList from './comics-back-list/ComicsBackList';
+import React, { useState, useEffect } from 'react';
 
-class ComicsBack extends Component {
+export default function ComicsBack() {
 
-	render(){
-		return(
-			<>
-				<section>
-					<header>
-						<h1>
-							Comics Back
-						</h1>
-					</header>
-					<ComicsBackForm/>
-					<ComicsBackList/>
-				</section>
-			</>
-		)
-	}
+	const [comicsFromDb, setGetComicsFromDb] = useState([]);
+
+	useEffect(() => {
+		obtenerComics()
+	}, []);
+
+	const obtenerComics = async () => {
+		const data = await getComics();
+		if (data) {
+			setGetComicsFromDb(data);
+		}
+	};
+
+	//borro el comic seleccionado y gestiono si se borró correctamente mediante el status, si es así
+	//actualizo el estado con el estado con la lista de comics actualizada
+	const handleDelete = (idToDelete) => {
+		if (comicsFromDb) {
+			eliminarComic(idToDelete)
+				.then((status) => {
+					if (status === 200) {
+						let comicsNuevos = comicsFromDb.filter((comic) => comic.id !== idToDelete);
+						setGetComicsFromDb(comicsNuevos);
+					}
+				});
+		}
+	};
+
+	return (
+		<>
+			<section>
+				<header>
+					<h1>
+						Comics Back
+					</h1>
+				</header>
+				<ComicsBackForm />
+				<ComicsBackList datosComics={comicsFromDb} handleDelete={handleDelete}/>
+			</section>
+		</>
+	)
 }
-
-export default ComicsBack;
--- src/backend/comics-back/comics-back-list/ComicsBackList.js
+++ src/backend/comics-back/comics-back-list/ComicsBackList.js
@@ -2,36 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './ComicsBackList.css';
 import { getComics, eliminarComic } from '../../../services/comics';
 
-export default function ComicsBackList() {
+export default function ComicsBackList(props) {
 
-	const [comicsFromDb, setGetComicsFromDb] = useState([]);
+	//extrae las propiedades que llega en props y las guarda en las variables
+	//estas tienen que llamarse igual que el prop para que lo encuentre
+	const {datosComics, handleDelete} = props;
 
-	//El use Effect es como el componente DidMount 
-	useEffect(() => {
-		// Obtener noticias al cargar el componente
-		obtenerComics()
-	}, [])
-
-	const obtenerComics = async () => {
-		const data = await getComics();
-		if (data) {
-			setGetComicsFromDb(data);
-		}
-	};
-
-	//borro el comic seleccionado y gestiono si se borró correctamente mediante el status, si es así
-	//actualizo el estado con el estado con la lista de comics actualizada
-	const handleDelete = (idToDelete) => {
-		if (comicsFromDb) {
-			eliminarComic(idToDelete)
-			.then((status) => {
-				if (status === 200) {
-					let comicsNuevos = comicsFromDb.filter((comic) =>  comic.id !== idToDelete );
-					setGetComicsFromDb(comicsNuevos);
-				}
-			});
-		}
-	};
+	
 
 	return (
 		<>
@@ -47,7 +24,7 @@ export default function ComicsBackList() {
 				</thead>
 				<tbody>
 					{
-						comicsFromDb.map((comic, index) => (
+						datosComics.map((comic, index) => (
 							<tr key={index}>
 								<td>{comic.title}</td>
 								<td>{comic.image}</td>
